3b3371c21d78d59905604050b4fb4d8aa1be8a2f
HaloSense <liuzj990412@live.com>
2022-03-15 01:37:55 -0400

    输出在遍历txt过程中跳过的行
    添加语句：如果在遍历每一行的过程中遇到非空行且不是需要的行（中-英或英-中格式），则写入到新txt文件并继续遍历。文件变动内有示例输出。

--- txt转其他格式/txt-to-csv.py
+++ txt转其他格式/txt-to-csv.py
@@ -10,7 +10,9 @@ def is_contains_chinese(strs):
             return True
     return False
 
-nameFile = open('New_Recourse.txt', 'r', encoding = 'utf-8')    # 以UTF-8格式打开txt
+nameFile = open('New_Recourse.txt', 'r', encoding = 'utf-8')        # 以UTF-8格式打开txt
+skippedLines = open('skipped_lines.txt', 'w', encoding = 'utf-8')   # 新建一个txt保存跳过的行
+
 curLine = ''
 cnList = []
 enList = []
@@ -18,13 +20,14 @@ enList = []
 for lines in nameFile:                      # 读取行
     curLine = nameFile.readline()
 
-    if not curLine:                         # 若文件结束，退出循环
+    if not curLine:                         # 若为空行，则直接跳转到下一行
         continue
 
-    curLine = curLine.replace('\n', '')     # 以制表符为分隔符分割每行
-    lineContent = curLine.split('\t')       # 转为list
+    curLine = curLine.replace('\n', '')     # 去除换行符
+    lineContent = curLine.split('\t')       # 以制表符为分隔符分割每行并转为list
 
-    if(len(lineContent) != 2):              # 若当前行的列表长度不为2（乱七八糟别的东西）则下一行
+    if(len(lineContent) != 2):              # 若当前行的列表长度不为2（明显不是中文-英文或英文-中文格式）则输出当前行到文件并跳到下一行
+        skippedLines.write(curLine + '\n')  
         continue
     
     # 检测每行两个元素是否包含中文，并用flags列表标记
@@ -36,9 +39,14 @@ for lines in nameFile:                      # 读取行
     elif(flags == [False, True]):       # 第二个元素有中文，第一个没有
         cnList.append(lineContent[1])
         enList.append(lineContent[0])
-    else:                               # 其他情况，直接跳下一行
+    else:                               # 其他情况，输出当前行到文件并跳到下一行
+        skippedLines.write(curLine + '\n')
         continue
 
 df = pd.DataFrame({'enName':enList, 'cnName':cnList})   # 转为pandas库的DataFrame
 
 df.to_csv("Translations.csv", index = False, sep = ',', encoding = 'utf-8-sig') # 按utf-8-sig编码写入csv，用Excel打开不乱码
+
+# 关闭两个文件
+nameFile.close()
+skippedLines.close()
