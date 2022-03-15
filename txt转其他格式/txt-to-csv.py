e38669c83cd6e670f09297cdf721cfbc9c42ebc2
HaloSense <liuzj990412@live.com>
2022-03-15 00:27:03 -0400

    添加注释

--- txt转其他格式/txt-to-csv.py
+++ txt转其他格式/txt-to-csv.py
@@ -10,34 +10,35 @@ def is_contains_chinese(strs):
             return True
     return False
 
-nameFile = open('New_Recourse.txt', 'r', encoding = 'utf-8')
+nameFile = open('New_Recourse.txt', 'r', encoding = 'utf-8')    # 以UTF-8格式打开txt
 curLine = ''
 cnList = []
 enList = []
 
-for lines in nameFile:
+for lines in nameFile:                      # 读取行
     curLine = nameFile.readline()
 
-    if not curLine:
+    if not curLine:                         # 若文件结束，退出循环
         continue
 
-    curLine = curLine.replace('\n', '')
-    lineContent = curLine.split('\t')
+    curLine = curLine.replace('\n', '')     # 以制表符为分隔符分割每行
+    lineContent = curLine.split('\t')       # 转为list
 
-    if(len(lineContent) != 2):
+    if(len(lineContent) != 2):              # 若当前行的列表长度不为2（乱七八糟别的东西）则下一行
         continue
     
-    if(is_contains_chinese(lineContent[0]) == True):
+    # 检测每行两个元素是否包含中文，并用flags列表标记
+    flags = [is_contains_chinese(lineContent[0]), is_contains_chinese(lineContent[1])]
+
+    if(flags == [True, False]):         # 第一个元素有中文，第二个没有
         cnList.append(lineContent[0])
         enList.append(lineContent[1])
-    else:
+    elif(flags == [False, True]):       # 第二个元素有中文，第一个没有
         cnList.append(lineContent[1])
         enList.append(lineContent[0])
+    else:                               # 其他情况，直接跳下一行
+        continue
 
-#dataNames = [enList, cnList]
-#print(len(dataNames[0]))
-#print(len(dataNames[1]))
+df = pd.DataFrame({'enName':enList, 'cnName':cnList})   # 转为pandas库的DataFrame
 
-df = pd.DataFrame({'enName':enList, 'cnName':cnList})
-
-df.to_csv("Translations.csv", index = False, sep = ',', encoding = 'utf-8-sig')
+df.to_csv("Translations.csv", index = False, sep = ',', encoding = 'utf-8-sig') # 按utf-8-sig编码写入csv，用Excel打开不乱码
